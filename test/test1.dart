f1cd37f9037b64008a7684796c5e73ee19b6de86
BOMHAI <61772881+bomhai@users.noreply.github.com>
2021-04-25 00:01:42 +0900

    Create test1.dart
    처음 테스트 ( row, column )

---
+++ test/test1.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			title: 'Simple',
+			home: Scaffold(
+				appBar: AppBar(
+					title: Text('Simple'),
+					),
+				body: Center(
+					child: Row(
+						children:<Widget>[
+							Column(
+								children:<Widget>[
+									Text('A'),
+									Text('B'),
+									Text('C'),
+									Text('D'),
+								],
+							),
+							Column(
+								children: <Widget>[
+									Text('E'),
+									Text('F'),
+									Text('G'),
+									Text('H'),
+								],
+							),
+							Column(
+								children: <Widget>[
+									Text('I'),
+									Text('J'),
+									Text('K'),
+									Text('L'),
+								],
+							),
+						]
+					),
+				),
+			),
+		);
+	}
+}
+
